7fa15253ef0df0183eef116a0514db7d01718631
trucanh <anhtructran111@gmail.com>
2021-09-08 17:22:00 +0700

    handle new page for website

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,6 +15,8 @@ ActiveStorage.start()
 require("jquery");
 require("packs/move_btn");
 
+
+
 function displayAudio(el, audio){
    if(el.matches("#id_choose_yes")){
     audio.play();
@@ -30,11 +32,15 @@ function displayAudio2(el, audio){
  };
 };
 
-function hideButton(){
+function handleHideButton(){
   var myBtn = document.getElementById("id_listen_event");
   var myText = document.getElementById("id_send_love");
   var groupBtn = document.getElementById("id_answer");
+  var mTexts = document.querySelectorAll(".m-text")
   console.log(groupBtn);
+  for (const mText of mTexts){
+    mText.classList.add("remove-text")
+  }
   myBtn.style.display = "none";
   myText.style.display = "block";
   groupBtn.style.display = "flex";
@@ -43,18 +49,17 @@ function hideButton(){
 
 $(document).ready(function(){
 
+
+
   $(document).on("click", '#id_choose_yes', function(event){
-    let audio = new Audio('https://assets.mixkit.co/sfx/preview/mixkit-retro-arcade-casino-notification-211.mp3');
+    let audio = new Audio('assets/button_click');
     let el = event.target;
     displayAudio(el, audio);
   });
 
   $(document).on("click", '#id_listen_event', function(event){
-    let audio = new Audio('https://cf-media.sndcdn.com/C6etsQpkYxnl.128.mp3?Policy=eyJTdGF0ZW1lbnQiOlt7IlJlc291cmNlIjoiKjovL2NmLW1lZGlhLnNuZGNkbi5jb20vQzZldHNRcGtZeG5sLjEyOC5tcDMqIiwiQ29uZGl0aW9uIjp7IkRhdGVMZXNzVGhhbiI6eyJBV1M6RXBvY2hUaW1lIjoxNjMwODE2OTA4fX19XX0_&Signature=NU0W1zzsoh90uRCKZl5KLkNFiTsexruHp69NocC~aFVt4lXutAThvLEchvQRQz7efIQE4H2m3gx~ZC08fk1Lm2WTpVh1mXtg4nbtscwsx6Mpv0Bt1ILSRH6vXg5blegYRrusoSTse82Nk7AVoN7cn2amIbm~pupEI5W8LNW-QYFbpyp2CDqN0hw-M23sbM9rHe-OsrBhwlSUt0w-x1geRtAG5bwOjKaxErPjFEKuaLMHvwER3-rQedI3TmA69sIRLnEeNRWKgsRMrMxe58vFDviS1jcjPxar43j0JB1zzp9CDejTONKvpqRGP2qXAK8NO6Ov4NoAkrlVUY8728pLAg__&Key-Pair-Id=APKAI6TU7MMXM5DG6EPQ');
-    let el = event.target;
-    displayAudio2(el,audio);
-    hideButton();
 
+    handleHideButton();
 
   });
 
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,5 +1,7 @@
 require "active_support/core_ext/integer/time"
 
+
+
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
@@ -73,4 +75,7 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  #added
+  config.assets.check_precompiled_asset = false
 end
